8cab4846d31c24c57b3611a593ffeeb47a91a1a1
Azim <azim.kazimli03@gmail.com>
2022-11-25 00:05:50 +0400

    UntestedV1

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,10 +1,24 @@
+import { Card } from './../board/card.model';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from './../user/user.service';
+import { UserCards } from './../user/user-card.model';
+import { BoardCards } from './../board/board-cards.model';
 import { MessagesService } from './messages.service';
 import { Module } from "@nestjs/common";
 import { MessagesGateway } from './messages.gateaway';
 import { BoardModule } from '../board/board.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/user/user.model';
+import { Board } from 'src/board/board.model';
+import { UsersModule } from 'src/user/user.module';
 
 @Module({
-    imports: [BoardModule],
+    imports: [BoardModule,
+    SequelizeModule.forFeature([User, Board, BoardCards, UserCards, Card]),
+    UsersModule,
+    BoardModule,
+    JwtModule
+    ],
     providers: [MessagesService, MessagesGateway]
 })
 export class MessagesModule { }
