63cb3283e503ee95a6b78c9ff05cd072930d9260
Azim Ka <azim.kazimli03@gmail.com>
2023-01-07 13:13:07 +0400

    adding emits

--- src/helper/helper.module.ts
+++ src/helper/helper.module.ts
@@ -3,9 +3,10 @@ import { Module } from "@nestjs/common";
 import { BoardCards } from "../board/board-cards.model";
 import { Board } from "../board/board.model";
 import { HelperService } from "./helper.service";
+import { UserCards } from '../user/user-card.model';
 
 @Module({
-    imports: [SequelizeModule.forFeature([BoardCards, Board,])],
+    imports: [SequelizeModule.forFeature([BoardCards, Board, UserCards])],
     providers: [HelperService],
     exports: [HelperService]
 
